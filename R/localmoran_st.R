8c22019d5d58602f2bea95b6757f54b475ce8c9d
Nils Tjaden <n.b.tjaden@utwente.nl>
2023-03-10 15:07:40 +0100

    documentation of core functions

--- R/localmoran_st.R
+++ R/localmoran_st.R
@@ -4,13 +4,87 @@
 # license: GPL-2 | GPL-3 [expanded from: GPL (≥ 2)]
 #
 
-#' @importFrom stats na.fail naresid
+#' Calculate spatio-temporal Moran's I
 #'
+#' Calculates a spatio-temporal version of local Moran's I (a.k.a
+#' spatio-temporal LISA) as proposed by Gao et al. (2019).
+#'
+#' @importFrom stats na.fail naresid
+#' @export
+#' @author Roger Bivand <Roger.Bivand@@nhh.no> (original code of spdep::moran)
+#' @author Jeff Sauer (conditional code)
+#' @author Levi Wolf (conditional code)
+#' @author Nils Tjaden <n.b.tjaden@@utwente.nl> (adjustments for spatio-temporal
+#'   use)
+#' @references Gao, Y. et al. (2019) ‘Measuring spatio-temporal autocorrelation
+#'   in time series data of collective human mobility’, Geo-spatial Information
+#'   Science, 22(3), pp. 166–173. doi:10.1080/10095020.2019.1643609.
+#' @references Anselin, L. 1995. Local indicators of spatial association,
+#'   Geographical Analysis, 27, 93–115
+#' @references Getis, A. and Ord, J. K. 1996 Local spatial statistics: an
+#'   overview. In P. Longley and M. Batty (eds) Spatial analysis: modelling in a
+#'   GIS environment (Cambridge: Geoinformation International), 261–277
+#' @references Sokal, R. R, Oden, N. L. and Thomson, B. A. 1998. Local Spatial
+#'   Autocorrelation in a Biological Model. Geographical Analysis, 30. 331–354
+#' @references Bivand RS, Wong DWS 2018 Comparing implementations of global and
+#'   local indicators of spatial association. TEST, 27(3), 716–748
+#'   doi:10.1007/s11749-018-0599-x
+#' @references Sauer, J., Oshan, T. M., Rey, S., & Wolf, L. J. 2021. The
+#'   Importance of Null Hypotheses: Understanding Differences in Local Moran’s
+#'   under Heteroskedasticity. Geographical Analysis. doi:10.1111/gean.12304
+#' @references Bivand, R. (2022), R Packages for Analyzing Spatial Data: A
+#'   Comparative Case Study with Areal Data. Geographical Analysis, 54(3),
+#'   488-518. doi:10.1111/gean.12319
+#'
+#' @param x a numeric matrix (or data.frame) of observations. Columns represent
+#'   space, rows represent time, so that each column contains a time series
+#'   measured at one location. The number of columns must equal the length of
+#'   the neighbors list in `listw`.
+#' @param listw a `listw` object (see documentation for `spdep`)
+#' @param zero.policy default NULL, use global option value; if TRUE assign zero
+#'   to the lagged value of zones without neighbours, if FALSE assign NA
+#' @param na.action a function (default `na.fail`), can also be na.omit or
+#'   na.exclude - in these cases the weights list will be subsetted to remove
+#'   NAs in the data. It may be necessary to set zero.policy to TRUE because
+#'   this subsetting may create no-neighbour observations. Note that only
+#'   weights lists created without using the glist argument to `nb2listw` may be
+#'   subsetted. If `na.pass` is used, zero is substituted for NA values in
+#'   calculating the spatial lag
+#' @param conditional default TRUE: expectation and variance are calculated
+#'   using the conditional randomization null (Sokal 1998 Eqs. A7 & A8).
+#'   Elaboration of these changes available in Sauer et al. (2021). If FALSE:
+#'   expectation and variance are calculated using the total randomization null
+#'   (Sokal 1998 Eqs. A3 & A4).
+#' @param conditional	default TRUE: expectation and variance are calculated
+#'   using the conditional randomization null (Sokal 1998 Eqs. A7 & A8).
+#'   Elaboration of these changes available in Sauer et al. (2021). If FALSE:
+#'   expectation and variance are calculated using the total randomization null
+#'   (Sokal 1998 Eqs. A3 & A4).
+#' @param alternative	a character string specifying the alternative hypothesis,
+#'   must be one of greater, less or two.sided (default).
+#' @param mlvar	default TRUE: values of local Moran's I are reported using the
+#'   variance of the variable of interest (sum of squared deviances over n), but
+#'   can be reported as the sample variance, dividing by (n-1) instead; both are
+#'   used in other implementations.
+#' @param spChk	should the data vector names be checked against the spatial
+#'   objects for identity integrity, TRUE, or FALSE, default NULL to use
+#'   get.spChkOption()
+#' @param adjust.x default FALSE, if TRUE, x values of observations with no
+#'   neighbours are omitted in the mean of x
+
+#' @returns `Ii_st` local moran statistic
+#' @returns `E.Ii_st` expectation of local moran statistic
+#' @returns `Var.Ii_st` variance of local moran statistic
+#' @returns `Z.Ii_st` standard deviate of local moran statistic
+#' @returns `Pr()_st` p-value of local moran statistic using pnorm()
+#' @returns In addition, an attribute data frame "quadr" indicating the
+#'   High-High etc. quadrants
+#'
+#' @examples
 localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
                           conditional=TRUE, alternative = "two.sided",
                           mlvar=TRUE, spChk=NULL, adjust.x=FALSE) {
   ## input checks
-  #stopifnot(is.vector(x))
   if (!inherits(listw, "listw")) # check if the weight list is a weights list
     stop(paste(deparse(substitute(listw)), "is not a listw object"))
   if (is.null(zero.policy))      # if no zero policy is defined, get it from the spdep options
@@ -31,14 +105,12 @@ localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
   x <- na.action(x)
   na.act <- attr(x, "na.action") # will be null if nothing was done to existing NA's or no NA's exist
 
-
   rn <- attr(listw, "region.id")
   if (!is.null(na.act)) { # if anything was done to the data when checking NA's, adjust weights list accordingly
     subset <- !(1:length(listw$neighbours) %in% na.act)
     listw <- subset(listw, subset, zero.policy=zero.policy)
     excl <- class(na.act) == "exclude"
   }
-
 
   n <- length(listw$neighbours)
   if (n != length(x))stop("Different numbers of observations")
@@ -79,7 +151,6 @@ localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
   quadr <- interaction(cut(z, c(-Inf, 0, Inf), labels=lbs),
                        cut(lz, c(-Inf, 0, Inf), labels=lbs), sep="-")
 
-
   # calculate variance of data (deviation from mean)
   if (mlvar) { # divide by n or n-1?
     if (adjust.x) { # omit x values of observations with no neighbors?
@@ -96,8 +167,6 @@ localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
   }
 
   # calculate spatio-temporal local Moran's I
-  # Ii
-  #res[,1] <- (z/s2) * lz
   Ii <- rep(x = NA, times=n)
   for(i in 1:n){
     neighbours <- listw$neighbours[[i]]
@@ -107,7 +176,6 @@ localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
   }
   res[,1] <- Ii
 
-
   # calculate expectation (E.i)
   Wi <- sapply(listw$weights, sum) # sum up the weights of all neighbours per location, results in either 0 or 1. See Sokal 1998 section 3.1
   if (conditional){	# use conditional randomization null rather than total randomization null (Sokal 1998 eq. A7 vs. A3). See also Sauer et al. (2021).
@@ -116,7 +184,6 @@ localmoran_st <- function(x, listw, zero.policy=NULL, na.action=na.fail,
   } else {
     res[, 2] <- -Wi / (n-1)
   }
-
 
   # calculate variance (Var.Ii)
   if (mlvar)  {# divide by n or n-1?
--- R/moran_st.R
+++ R/moran_st.R
@@ -3,23 +3,46 @@
 # package version: 1.2.2
 # license: GPL-2 | GPL-3 [expanded from: GPL (≥ 2)]
 
-
+#' Calculate spatio-temporal Moran's I
+#'
+#' Calculates a spatio-temporal version of (global) Moran's I as proposed by Gao
+#' et al. (2019). Called by `moran.test_st`.
+#'
+#' @export
+#' @author Roger Bivand <Roger.Bivand@@nhh.no> (original code of spdep::moran)
+#' @author Nils Tjaden <n.b.tjaden@@utwente.nl> (adjustments for spatio-temporal
+#'   use)
+#' @references Gao, Y. et al. (2019) ‘Measuring spatio-temporal autocorrelation
+#'   in time series data of collective human mobility’, Geo-spatial Information
+#'   Science, 22(3), pp. 166–173. doi:10.1080/10095020.2019.1643609.
+#'
+#' @param x a numeric matrix (or data.frame) of observations. Columns represent
+#'   space, rows represent time, so that each column contains a time series
+#'   measured at one location. The number of columns must equal the length of
+#'   the neighbors list in `listw`.
+#' @param listw a `listw` object (see documentation for `spdep`)
+#' @param n number of zones
+#' @param S0 global sum of weights
+#' @param zero.policy default NULL, use global option value; if TRUE assign zero
+#'   to the lagged value of zones without neighbours, if FALSE assign NA
+#' @param NAOK 	if 'TRUE' then any 'NA' or 'NaN' or 'Inf' values in x are passed
+#'   on to the foreign function. If 'FALSE', the presence of 'NA' or 'NaN' or
+#'   'Inf' values is regarded as an error.
+#' @returns  A list: `I` spatio-temporal Moran's I `K` sample kurtosis of x
+#' @examples
 moran_st <- function (x, listw, n, S0, zero.policy = NULL, NAOK = FALSE){
   if (is.null(zero.policy))
     zero.policy <- spdep::get.ZeroPolicyOption()
   stopifnot(is.logical(zero.policy))
   n1 <- length(listw$neighbours)
   w <- spdep::listw2mat(listw = listw)
-  #x <- c(x)
   if (n1 != length(x))
     stop("objects of different length")
   xx <- rowMeans(x, na.rm=NAOK)
-  #z <- x - xx
   z <- rep(x = NA, times=n1)
   for(i in 1:n1){
     z[i] <- ts_deviation(X=x[,i], Y=xx)
   }
-
 
   zz <- sum(z^2, na.rm = NAOK)
   K <- (length(x) * sum(z^4, na.rm = NAOK))/(zz^2)
@@ -31,7 +54,56 @@ moran_st <- function (x, listw, n, S0, zero.policy = NULL, NAOK = FALSE){
   res
 }
 
-
+#' Moran's I test for spatial autocorrelation (spatio-temporal)
+#'
+#' This is a 1:1 copy of `spdep::moran.test` with minor adjustments for use with
+#' `moran_st`. See `?spdep::moran.test` for more information.
+#' @export
+#'
+#' @author Roger Bivand <Roger.Bivand@@nhh.no> (original code of
+#'   spdep::moran.test)
+#' @author Nils Tjaden <n.b.tjaden@@utwente.nl> (minor adjustments for
+#'   spatio-temporal use)
+#' @references Cliff, A. D., Ord, J. K. 1981 Spatial processes, Pion
+#' @param x a numeric matrix (or data.frame) of observations. Columns represent
+#'   space, rows represent time, so that each column contains a time series
+#'   measured at one location. The number of columns must equal the length of
+#'   the neighbors list in `listw`.
+#' @param listw a `listw` object (see documentation for `spdep`)
+#' @param randomisation variance of I calculated under the assumption of
+#'   randomisation, if FALSE normality
+#' @param zero.policy default NULL, use global option value; if TRUE assign zero
+#'   to the lagged value of zones without neighbours, if FALSE assign NA
+#' @param alternative a character string specifying the alternative hypothesis,
+#'   must be one of greater (default), less or two.sided.
+#' @param rank logical value - default FALSE for continuous variables, if TRUE,
+#'   uses the adaptation of Moran's I for ranks suggested by Cliff and Ord
+#'   (1981, p. 46)
+#' @param na.action a function (default `na.fail`), can also be na.omit or
+#'   na.exclude - in these cases the weights list will be subsetted to remove
+#'   NAs in the data. It may be necessary to set zero.policy to TRUE because
+#'   this subsetting may create no-neighbour observations. Note that only
+#'   weights lists created without using the glist argument to `nb2listw` may be
+#'   subsetted. If `na.pass` is used, zero is substituted for NA values in
+#'   calculating the spatial lag
+#' @param adjust.n default TRUE, if FALSE the number of observations is not
+#' adjusted for no-neighbour observations, if TRUE, the number of observations
+#' is adjusted
+#' @param spChk	should the data vector names be checked against the spatial
+#'   objects for identity integrity, TRUE, or FALSE, default NULL to use
+#'   `get.spChkOption()`
+#' @param drop.EI2	default FALSE, if TRUE, emulate CrimeStat <= 4.02
+#' @returns A list with class htest containing the following components:
+#' @returns `statistic` the value of the standard deviate of Moran's I.
+#' @returns `p.value` the p-value of the test.
+#' @returns `estimate`	the value of the observed Moran's I, its expectation and
+#'   variance under the method assumption.
+#' @returns `alternative` a character string describing the alternative
+#'   hypothesis.
+#' @returns `method`	a character string giving the assumption used for
+#'   calculating the standard deviate.
+#' @returns `data.name` a character string giving the name(s) of the data.
+#' @examples
 moran.test_st <- function(x, listw, randomisation=TRUE, zero.policy=NULL,
                           alternative="greater", rank = FALSE, na.action=na.fail, spChk=NULL,
                           adjust.n=TRUE, drop.EI2=FALSE) {
